5a721c4759ba59366dd0a76ad1d384aa5e0caad4
dk7648 <dk7648@korea.ac.kr>
2025-03-08 16:46:10 +0900

    part2: 이미지 넣는 법 & public 폴더 이용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Nav, Navbar, Container } from "react-bootstrap";
+import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 
 import "./App.css";
 
@@ -17,6 +17,28 @@ function App() {
         </Container>
       </Navbar>
 
+      <div className="main-bg"></div>
+      <Container>
+
+      
+      <Row>
+        <Col sm>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col sm>
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col sm>
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+      </Row>
+    </Container>
       <Button variant="primary">Primary</Button>
     </div>
   );
