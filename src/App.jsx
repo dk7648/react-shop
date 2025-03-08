1cf661dce1f4122aa5fa186d855c9d3658d15b3b
dk7648 <dk7648@korea.ac.kr>
2025-03-08 13:57:35 +0900

    part2: 새로운 프로젝트 생성 & Bootstrap 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Nav, Navbar, Container } from "react-bootstrap";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-      
-    </>
-  )
+    <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
