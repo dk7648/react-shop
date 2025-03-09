cd0fb2027e67ae32c793e75eb6fde2ff56f826e2
dk7648 <dk7648@korea.ac.kr>
2025-03-09 23:28:34 +0900

    part2: 코드 길어지면 import export 하면 됩니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 
+import data from "./data.js";
 import "./App.css";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -19,29 +23,30 @@ function App() {
 
       <div className="main-bg"></div>
       <Container>
-
-      
-      <Row>
-        <Col sm>
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </Col>
-        <Col sm>
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </Col>
-        <Col sm>
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          {shoes.map((target, i) => {
+            return <Card key={i} i={i} shoes={target} />;
+          })}
+        </Row>
+      </Container>
       <Button variant="primary">Primary</Button>
     </div>
   );
 }
 
+function Card(props) {
+  return (
+    <Col sm>
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </Col>
+  );
+}
 export default App;
