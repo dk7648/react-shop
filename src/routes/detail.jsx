7773bdfdb1dd5a0f416ec8492d4f1df7dd4fec08
dk7648 <dk7648@korea.ac.kr>
2025-03-12 08:56:09 +0900

    part2: Lifecycle과 useEffect

--- src/routes/detail.jsx
+++ src/routes/detail.jsx
@@ -1,27 +1,43 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from 'styled-components';
-
-let StyledButton = styled.button`
-  background : ${ props => props.bg };
-  color : ${props => props.bg=='blue' ? 'white' : 'black'};
-  padding : 10px;
-`
-
 function Detail(props) {
+  useEffect(() => {
+    console.log(1)
+    let timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000)
+    return () => {
+      console.log(2);
+      clearTimeout(timer)
+    }
+  }, []);
+
+  let [input, setInput] = useState('');
+  let [alert, setAlert] = useState(true);
   let { id } = useParams();
-  let target = props.shoes.find((e) => e.id == id)
+  let target = props.shoes.find((e) => e.id == id);
   return (
     <div className="container">
-      <StyledButton bg="blue">버튼입니다</StyledButton>
+      {alert ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
-            src={"https://codingapple1.github.io/shop/shoes"+(target.id+1)+".jpg"}
+            src={
+              "https://codingapple1.github.io/shop/shoes" +
+              (target.id + 1) +
+              ".jpg"
+            }
             width="100%"
           />
         </div>
         <div className="col-md-6">
-
+          {
+            isNaN(input) ? <p>경고: 숫자만 입력해주세요</p> :
+            <p>{input}</p>
+          }
+          <input type="text" onChange={(e) => {setInput(e.target.value)}}/>
           <h4 className="pt-5">{target.title}</h4>
           <p>{target.content}</p>
           <p>{target.price}원</p>
