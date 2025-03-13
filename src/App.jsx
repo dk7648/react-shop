4bd9692fa6dd9aebe020ae945bdc64a6e469d95f
dk7648 <dk7648@korea.ac.kr>
2025-03-13 21:53:01 +0900

    part2: 리액트에서 탭 UI 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,120 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import data from "./data.js";
 import Detail from "./routes/detail.jsx";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
+  let [btnCounter, setBtnCounter] = useState(0);
+  console.log(typeof btnCounter);
+  let [loading, setLoading] = useState(false);
   let navigate = useNavigate();
   return (
     <div className="App">
-      
-
-
-
-
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand onClick={()=>{navigate('/')}}>Shop</Navbar.Brand>
+          <Navbar.Brand
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Shop
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              About
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
-        <Route path="/" element={
-          <>
-            <div className="main-bg"></div>
+        <Route
+          path="/"
+          element={
+            <>
+              {loading ? (
+                <div className="alert alert-warning">로딩중</div>
+              ) : null}
+
+              <div className="main-bg"></div>
               <Container>
                 <Row>
                   {shoes.map((target, i) => {
-                    return <Card key={i} i={i} shoes={target} />;
+                    return <Card navigate={navigate} key={i} shoes={target} />;
                   })}
                 </Row>
               </Container>
-            <Button variant="primary">Primary</Button>
-          </>
-        }/>
-        <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
-        <Route path="/about" element={<About/>}>
-          <Route path="member" element={<h4>회사 멤버</h4>}/>
-          <Route path="location" element={<h4>회사 위치</h4>}/>
+              <Button
+                onClick={() => {
+                  setLoading(true);
+                  console.log(btnCounter);
+                  axios
+                    .get(
+                      "https://codingapple1.github.io/shop/data" +
+                        (btnCounter + 2) +
+                        ".json"
+                    )
+                    .then((result) => {
+                      let copy = [...shoes, ...result.data];
+                      setShoes(copy);
+                      setBtnCounter(btnCounter + 1);
+                      setLoading(false);
+                    })
+                    .catch(() => {
+                      alert("데이터 받기 실패 ㅠㅠ");
+                      setLoading(false);
+                    });
+                }}
+                variant="primary"
+              >
+                데이터 받기
+              </Button>
+            </>
+          }
+        />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<h4>회사 멤버</h4>} />
+          <Route path="location" element={<h4>회사 위치</h4>} />
         </Route>
 
-        <Route path="/event" element={<div><h4>오늘의 이벤트</h4><Outlet/></div>}>
-          <Route path="one" element={<h4>첫 주문시 양배추즙 서비스</h4>}/>
-          <Route path="two" element={<h4>생일 기념 쿠폰 받기</h4>}/>
+        <Route
+          path="/event"
+          element={
+            <div>
+              <h4>오늘의 이벤트</h4>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path="one" element={<h4>첫 주문시 양배추즙 서비스</h4>} />
+          <Route path="two" element={<h4>생일 기념 쿠폰 받기</h4>} />
         </Route>
 
-        <Route path="*" element={<>없는 페이지입니다.</>}/>
+        <Route path="*" element={<>없는 페이지입니다.</>} />
       </Routes>
     </div>
   );
@@ -63,18 +125,20 @@ function About() {
       <h4>회사정보</h4>
       <Outlet></Outlet>
     </div>
-  )
+  );
 }
 function Card(props) {
   return (
     <Col sm>
-      <img
+      <a href={"/detail/"+props.shoes.id}><img
         src={
-          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+          "https://codingapple1.github.io/shop/shoes" +
+          (props.shoes.id + 1) +
+          ".jpg"
         }
         width="80%"
       />
-      <h4>{props.shoes.title}</h4>
+      <h4>{props.shoes.title}</h4></a>
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
     </Col>
--- src/routes/detail.jsx
+++ src/routes/detail.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 function Detail(props) {
-  let [input, setInput] = useState('');
+  let [input, setInput] = useState("");
   let [Alert, setAlert] = useState(true);
+  let [tap, setTap] = useState(0);
   let { id } = useParams();
   let target = props.shoes.find((e) => e.id == id);
-  
+
   useEffect(() => {
-    console.log(1)
+    console.log(1);
     let timer = setTimeout(() => {
       setAlert(false);
-    }, 2000)
-    
+    }, 2000);
+
     return () => {
       console.log(2);
-      clearTimeout(timer)
-    }
+      clearTimeout(timer);
+    };
   }, []);
 
-  useEffect(()=> {
-      if(isNaN(input)) {
-        alert("숫자만 입력하세요")
-      }
-  }, [input])
+  useEffect(() => {
+    if (isNaN(input)) {
+      alert("숫자만 입력하세요");
+    }
+  }, [input]);
 
   return (
     <div className="container">
@@ -41,18 +43,38 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
-          {
-            
-          }
-          <input type="text" onChange={(e) => {setInput(e.target.value)}}/>
+          <input
+            type="text"
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
           <h4 className="pt-5">{target.title}</h4>
           <p>{target.content}</p>
           <p>{target.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link eventKey="link0" onClick={()=>{setTap(0)}}>버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link1" onClick={()=>{setTap(1)}}>버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2" onClick={()=>{setTap(2)}}>버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TapContent tap={tap}/>
     </div>
+    
   );
 }
+
+function TapContent(props) {
+    return [<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][props.tap]
+}
+
 
 export default Detail;
